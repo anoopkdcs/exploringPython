8a00abaa23be6c5e46411310a1f2bd5161aabbfd
Anoop k <anoopk_dcs@uoc.ac.in>
2017-11-18 16:19:43 +0530

    Add files via upload

---
+++ programs/Visualization/multiline.py
@@ -0,0 +1,7 @@
+name_one="Anoop"
+name_two="Lakshmi"
+name_three="Abhi Ram"
+names= name_one + \
+        name_two + \
+        name_three
+print names
---
+++ programs/Visualization/visual.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 23:59:36 2017
+
+@author: anoop
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4], [1,4,9,16], 'ro') #g^ another style
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+
+
+
