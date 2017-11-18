3fe6d466bb544c4911a7045a35e6b3c139483f05
Manjary P Gangan <33519702+manjaryp@users.noreply.github.com>
2017-11-18 12:46:50 +0530

    Add files via upload

---
+++ programs/loop/for.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 19:27:51 2017
+
+@author: manjary
+"""
+
+num = 10
+for i in range(1,10):
+    print(i)
---
+++ programs/loop/while.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 19:31:30 2017
+
+@author: manjary
+"""
+
+count=0
+limit = int(input("Enter a limit"))
+while(count<=limit):
+	print(count)
+	count=count+1
+print("Thanks for using me!")
