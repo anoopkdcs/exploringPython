58602c85d1494f0c5746177118c7adbe963e0c4b
Anoop k <anoopk_dcs@uoc.ac.in>
2017-11-18 16:17:31 +0530

    Add files via upload

---
+++ programs/function/function_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 15 21:23:42 2017
+
+@author: anoop
+"""
+
+def printme(name):
+    print (name)
+    
+    
+a=10
+b=20
+c=a+b
+print (c)
+printme("anoop")        
